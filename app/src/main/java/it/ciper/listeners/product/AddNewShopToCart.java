5c10c1332cb9caf4a3eb51476ab8854242465b8c
Daniele Cipollone <dan.cip00@gmail.com>
2021-09-22 12:22:07 +0200

    LogIn and SignIn fixed

---
+++ app/src/main/java/it/ciper/listeners/product/AddNewShopToCart.java
@@ -0,0 +1,10 @@
+package it.ciper.listeners.product;
+
+import android.view.View;
+
+public class AddNewShopToCart implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
